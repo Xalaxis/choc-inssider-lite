62d568b60c3cc00acdf3f50af4d66b7b1b230567
Matthew Wolffsohn <matthew@wolfie.ovh>
2019-02-12 18:11:51 +0000

    Initial progress

---
+++ description.md
@@ -0,0 +1,7 @@
+inSSIDer: Visualize Your WiFi Environment
+
+Every WiFi network faces WiFi problems. And diagnosing and fixing those problems is difficult when you can't see them. inSSIDer gives you the insight you need to visualize your WiFi environment and discover conflicts, so you can create and maintain awesome WiFi for even the most demanding users.
+
+---
+
+Online account required for use.
--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -15,31 +15,27 @@ $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 #$fileLocation = '\\SHARE_LOCATION\to\INSTALLER_FILE'
 # Community Repo: Use official urls for non-redist binaries or redist where total package size is over 200MB
 # Internal/Organization: Download from internal location (internet sources are unreliable)
-$url        = '' # download url, HTTPS preferred
-$url64      = '' # 64bit URL here (HTTPS preferred) or remove - if installer contains both (very rare), use $url
+$url        = 'https://metageek.link/inssider-update-url-v1-0-2/inSSIDerSetup.exe' # download url, HTTPS preferred
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  fileType      = 'EXE_MSI_OR_MSU' #only one of these: exe, msi, msu
+  fileType      = 'EXE' #only one of these: exe, msi, msu
   url           = $url
-  url64bit      = $url64
   #file         = $fileLocation
 
-  softwareName  = 'inssider-lite*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  softwareName  = 'inSSIDer' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
 
   # Checksums are now required as of 0.10.0.
   # To determine checksums, you can get that from the original site if provided. 
   # You can also use checksum.exe (choco install checksum) and use it 
   # e.g. checksum -t sha256 -f path\to\file
-  checksum      = ''
+  checksum      = 'C3643B93118D0DF454298285E7C553470061F157810B705D747181C0E861CAE5'
   checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
-  checksum64    = ''
-  checksumType64= 'sha256' #default is checksumType
 
   # MSI
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
-  validExitCodes= @(0, 3010, 1641)
+  # silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  # validExitCodes= @(0, 3010, 1641)
   # OTHERS
   # Uncomment matching EXE type (sorted by most to least common)
   #silentArgs   = '/S'           # NSIS
